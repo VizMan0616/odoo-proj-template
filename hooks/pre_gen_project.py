f12246f9d3c3e2d8a93adb9b133c4bf2585471f7
José Vizcaya <60019108+VizMan0616@users.noreply.github.com>
2025-05-29 22:38:42 -0400

    feat: Several improvements to the script (#19)

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,5 @@
 import sys
 import subprocess
-import configparser
 
 
 def execute_git(*args) -> str:
@@ -10,6 +9,7 @@ def execute_git(*args) -> str:
     except Exception as ex:
         return True, ex.stderr.decode("utf-8")
 
+
 odoo_version = "{{ cookiecutter.odoo_version }}"
 ubuntu_release = "{{ cookiecutter.ubuntu_release }}"
 repository = "git@{{ cookiecutter.git_server }}:{{ cookiecutter.github_user }}/{{ cookiecutter.github_repo }}.git"
@@ -18,7 +18,8 @@ if odoo_version in ["16.0", "17.0"] and ubuntu_release in ["bionic", "focal"]:
     print("Error: Odoo 16.0 should be used with Ubuntu 22.04 (Jammy Jellyfish)")
     sys.exit(1)
 
-err, output = execute_git("ls-remote", repository)
-if err:
-    print(output)
-    sys.exit(1)
+if "{{ cookiecutter.github_user }}" != "ExampleUser" and "{{ cookiecutter.github_repo }}" != "example-repo":
+    err, output = execute_git("ls-remote", repository)
+    if err:
+        print(output)
+        sys.exit(1)
